15c7bd34e1acc2258d94b01d55899ea30794fafc
Polina Shevtsova <polinasevcova@MacBook-Air.local>
2021-03-24 13:21:11 +0300

    исправления после 1 ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,20 @@
 let popup = document.querySelector('.popup');
-let openPopupBtn = document.getElementById('edit-btn');
+let openPopupBtn = document.querySelector('.profile__edit-button');
 let closePopupBtn = document.querySelector('.popup__close-btn');
 let currentName = document.querySelector('.profile__title');
 let currentDescription = document.querySelector('.profile__subtitle');
-let newName = document.querySelector('.popup__full-name-input');
-let newDescription = document.querySelector('.popup__about-you-input');
+let newName = document.getElementById('full-name_input')
+let newDescription = document.getElementById('about-you__input');
 let popupForm = document.querySelector('.popup__form');
 
 function openPopup() {
     newName.value = currentName.textContent;
     newDescription.value = currentDescription.textContent;
-    popup.classList.add('popup__opened');
+    popup.classList.add('popup_opened');
 }
 
 function closePopup() {
-    popup.classList.remove('popup__opened');
+    popup.classList.remove('popup_opened');
 }
 
 function formSubmitHandler (evt) {
